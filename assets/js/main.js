d147a232057c8a7ef6888cbc57e598a0b9eac3b6
Lancelotti-beta <brunojosemarques.pe@gmail.com>
2023-03-21 22:04:46 -0300

    corrigindo main.js
     Arrumando o nome do objeto e do parametro | o erro anterior era ter
     adicionaod o nome da variavel no singular quando se foi criada no
     plural e do parametro que estava atribuindo "De" ao inves "Do"

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
-const $input = document.querySelectorAll("input[data-input]")
+const $input = document.querySelectorAll("[data-input]")
+const $button = document.querySelector(".botao--form")
 
 $input.forEach(elemento => {
     elemento.addEventListener('blur', input => {
@@ -8,15 +9,15 @@ $input.forEach(elemento => {
 
 function validaInput(input) {
     let inputAtual = input.dataset.input
-    
-    if(!input.validity.valid){
+
+    if (!input.validity.valid) {
         console.log('erro')
         document.querySelector(`label[for="${inputAtual}"]`).innerHTML = mostraMensagemDeErro(inputAtual, input)
     } else {
-	document.querySelector(`label[for="${inputAtual}"]`).innerHTML = "";
+        document.querySelector(`label[for="${inputAtual}"]`).innerHTML = "";
     }
 
-    
+
 }
 
 const tipoDeErros = [
@@ -26,28 +27,28 @@ const tipoDeErros = [
 ]
 
 const mensagensDeErros = {
-    nome : {
-        valueMissing :`Este campo não pode estar vazio! Favor adicionar um nome.`
+    nome: {
+        valueMissing: `Este campo não pode estar vazio! Favor adicionar um nome.`
     },
     email: {
-	valueMissing: `Este campo não pode estar vazio! Favor adicionar um email.`,
-	patternMismatch: `O email deve seguir um padrão com user@dominio.com`,
-	typeMismatch: `O email digitado não é valido, adicione um email valido.` 
+        valueMissing: `Este campo não pode estar vazio! Favor adicionar um email.`,
+        patternMismatch: `O email deve seguir um padrão com user@dominio.com`,
+        typeMismatch: `O email digitado não é valido, adicione um email valido.`
     },
     assunto: {
-	valueMissing: `Este campo não pode estar vazio! Favor adicionar um assunto.`
+        valueMissing: `Este campo não pode estar vazio! Favor adicionar um assunto.`
     },
     mensagem: {
-	valueMissing: `A mensagem não pode estar vazia.`
+        valueMissing: `A mensagem não pode estar vazia.`
     }
 }
 
 function mostraMensagemDeErro(tipoDoInput, input) {
     let mensagemDoErro = "";
-    
+
     tipoDeErros.forEach(erro => {
-        if(input.validity[erro])
-	    mensagemDoErro = mensagensDeErro[tipoDeInput][erro]);
+        if (input.validity[erro])
+            mensagemDoErro = mensagensDeErros[tipoDoInput][erro];
     });
 
     return mensagemDoErro;
